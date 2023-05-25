f241dacef5e02b0b80d4f2b65defb3e9bdb4da81
Tathya551 <tathya551@gmail.com>
2023-05-25 23:42:24 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f99bd1e6-ae18-4ce4-b25b-abc5ee5b7b7c';
+export const MODAL_CLOSE_SEC = 2.5;
